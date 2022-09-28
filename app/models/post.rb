2bbf0035a6c4aaca8bb1d9c4284624e6c27c549d
Unyielding1 <david.verde.alvarez@gmail.com>
2022-09-28 15:04:47 -0500

    create users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author_id, class_name: "User"
-  has_many :comments, 
-  has_many :likes,
+  belongs_to :author, :author_id, class_name: "User"
+  has_many :comments
+  has_many :likes
 end
