30d3df57df18252dcaad35f2ce79a71c8c06a5e1
Unyielding1 <david.verde.alvarez@gmail.com>
2022-09-28 17:54:05 -0500

    create methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :post
-  belongs_to :author_id, class_name: 'User'
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_like_counter
+
+  def update_like_counter
+    post.increment!(:likes_count)
+  end
+
+  private :update_like_counter
+
 end
