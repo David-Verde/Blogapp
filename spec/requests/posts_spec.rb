0870bc5d73be5fa21201c8413ac7a78aa4b4c66e
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-05 11:31:01 -0500

    create test for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /id' do
+    it 'returns http success' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  it 'Renders the index view' do
+    get '/users/:user_id/posts'
+    expect(response).to render_template(:index)
+  end
+
+  it 'Renders the show view' do
+    get '/users/:user_id/posts/:id'
+    expect(response).to render_template(:show)
+  end
+
+  it 'Returns http success' do
+    get '/users/id'
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'Renders template with correct text' do
+    get '/users'
+    expect(response.body).to include('Users index view')
+  end
+
+  it 'Renders template index' do
+    get '/users'
+    expect(response).to render_template(:index)
   end
 end
+
+describe 'GET /show' do
+  it 'Response status is ok' do
+    get users_path(1)
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'Renders the show view' do
+    get '/users/1/posts/1'
+    expect(response).to render_template(:show)
+  end
+
+end
+
+
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'Response status is 200' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'Renders the index view template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'Rendered view includes correct placeholder text' do
+      get users_path
+      expect(response.body).to include('Users index view')
+    end
+
+    it 'Returns http success' do
+      get '/users/id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Renders template with correct text' do
+      get '/users'
+      expect(response.body).to include('Users index view')
+    end
+
+    it 'Renders template index' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'Response status is ok' do
+      get users_path(1)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Renders the show view' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
   end
 end
