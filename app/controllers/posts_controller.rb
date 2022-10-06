889001333cad96c11c03cc5f63547b159515a2b6
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-06 11:04:28 -0500

    fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
-  def index; 
-  @user = User.find(params[:user_id])
-  @posts = @user.posts
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
-  def show;
 
-  @user = User.find(params[:user_id])
-  @post = Post.find(params[:id])
-  @comments = Comment.where(post_id: params[:id])
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: params[:id])
+  end
 end
-end
-
