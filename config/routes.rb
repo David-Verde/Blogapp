445c650aa609d24f423446704cede0590c2d4de8
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-07 10:44:44 -0500

    create forms for likes, comments and refactor post and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
  resources :users, only: [:index, :show] do
- resources :posts, only: [:index, :show]
+ resources :posts, only: [:index, :new, :create, :show] do
+    resources :likes, only: [:create]
+    resources :comments, only: [:new, :create]
+ end
 end
 root 'users#index'
 end
