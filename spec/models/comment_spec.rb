451cd70458e613e7f8090f427816190aa7537a99
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-11 13:19:32 -0500

    fix previewer sigestions

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,23 @@ RSpec.describe Comment, type: :model do
   post = Post.new(title: 'example', text: 'example', author: user)
   comment = Comment.new(author: user, post:)
 
-  it 'new comment is valid' do
-    expect(comment).to be_valid
+  it 'comment is not valid without author' do
+    comment.author = nil
+    expect(comment).to_not be_valid
+  end
+
+  it 'comment is not valid without post' do
+    comment.post = nil
+    expect(comment).to_not be_valid
+  end
+
+  it 'comment is not valid with text longer than 250 characters' do
+    comment.text = 'a' * 251
+    expect(comment).to_not be_valid
+  end
+
+  it 'comment is not valid with text shorter than 5 characters' do
+    comment.text = 'a' * 4
+    expect(comment).to_not be_valid
   end
 end
